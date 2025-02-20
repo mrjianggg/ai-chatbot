69dea73fb7a1f5cd4d743f6cdcdf578a8018be17
mrjianggg <972577493@qq.com>
2025-02-20 15:03:23 +0800

    test

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,8 @@
 module.exports = {
   apps: [{
     name: "chatbot",
-    script: "authbind --deep pnpm start",  // 关键修改
+    script: "authbind --deep pnpm start",
+    interpreter: "none", // 关键！禁止PM2使用默认解释器
     env: {
       NEXTAUTH_URL: "http://34.92.13.95:10808",
       PORT: 80,
